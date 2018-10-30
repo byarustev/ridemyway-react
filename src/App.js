c000b1f6ac92392b74ee7cf2dd4663aaf91831a5
byarustev <byarus45@gmail.com>
2018-10-30 13:48:57 +0300

    chore(routers): setup react routers - set up react router
    [finishes #161581837]

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+const Index = () => <h2>Home</h2>;
+const Test2 = () => <h2>test</h2>;
+const Header=()=>(
+    <nav>
+        <ul>
+            <li>
+                <a href='/'>Home</a>
+            </li>
+            <li>
+                <a href='/test/'>Test</a>
+            </li>
+
+        </ul>
+    </nav>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+          <div>
+          <Route path="/" exact component={Index} />
+          <Route path="/test" exact component={Test2} />
+          <Header/>
+          </div>
+      </Router>
     );
   }
 }
